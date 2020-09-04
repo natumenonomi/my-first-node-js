322a66316ce8a1cf2cdc7bcbe7b1b0fa9d2c9019
natumenonomi <nonegemu3e@gmail.com>
2020-09-05 01:15:24 +0900

    コメントアウトで説明をつけました

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-'use strict';
+'use strict';//Strict（厳格）モード
 const number = process.argv[2] || 0;
-let sum = 0;
-for (let i = 1; i <= number; i++) {
-  sum = sum + i;
+/*numberにprocsee.argv[2]＝デフォルトで用意されたファイルの中の引数のValueの２番目の値を代入。ORで。もし空の場合は0を代入(undefindにしないため）（選択的代入）*/
+let sum = 0;//初期値は0
+for (let i = 1; i <= number; i++) {//iが１でiが１ずつ増えていく　numberと同じになったら停止
+  sum = sum + i;//sumにiが足される　1+2+3+ …
 }
-console.log(sum);
+console.log(sum);//sumを出す
